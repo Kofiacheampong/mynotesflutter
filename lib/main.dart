d2bd54e29c10b8208c08e7bd53270ec6a3d636aa
Kofiacheampong <acheampong0805@outlook.com>
2024-03-05 13:54:42 -0600

    Step 3

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ void main() async {
       home: const HomePage(),
       routes: {
         '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView()
+        '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView()
       },
     ),
   );
